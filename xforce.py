ddf3efd87215a8e3795fc15cead6b350f2e63e6e
devargaslucas <117997944+devargaslucas@users.noreply.github.com>
2022-11-11 11:09:22 -0300

    Update xforce.py

--- xforce.py
+++ xforce.py
@@ -11,7 +11,7 @@ url = "https://api.xforce.ibmcloud.com/api/url/" + domain
 
 headers = {
     "Accept": "application/json",
-    "Authorization": "Basic NjY2NjQxYjItYmVjZi00NjBkLTg1ZjAtYThmMTM0YTM3YmZlOmRlOTRjNjljLWUxZGItNGQ4Yi05MDEyLTBjNDU4ZDkwMzY3ZQ=="
+    "Authorization": "***"
 }
 
 response = requests.get(url, headers=headers)
@@ -22,14 +22,6 @@ while True:
         saida = retorno['result']
         score = saida['score']
 
-        ########## HTTP Status #############
-        #conn = http.client.HTTPConnection(domain)
-        #conn.request("GET", "/")
-        # fgr1 = conn.getresponse()
-        #print(r1.status, r1.reason)
-
-        ########## Saidas #############
-        #print("Http Status:", r1.status)
         print("Domain", domain, "Score:", score)
         break
     except KeyError:
